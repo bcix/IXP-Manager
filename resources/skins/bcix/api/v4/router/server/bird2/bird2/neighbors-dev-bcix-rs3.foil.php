903fe1cb465a99bbcc3303de29e1d5e243c8bd5f
Christopher Rössler <caroessler@aol.com>
2025-06-13 12:03:15 +0200

    implemented otc flag and routeleak community

--- resources/skins/bcix/api/v4/router/server/bird2/bird2/neighbors-dev-bcix-rs3.foil.php
+++ resources/skins/bcix/api/v4/router/server/bird2/bird2/neighbors-dev-bcix-rs3.foil.php
@@ -96,6 +96,18 @@ int set allas;
         bgp_large_community.add( IXP_LC_FILTERED_PREFIX_LEN_TOO_LONG );
         accept;
     }
+#########
+# RFC9234
+#########
+<?php if( $int['autsys'] == 213973 ): // BCIX OUTREACH  ?>
+	if ! defined( bgp_otc ) then {
+        bgp_otc = 213973;
+    }
+<?php else: ?>
+	if defined( bgp_otc ) then {
+        bgp_large_community.add( IXP_LC_FILTERED_ROUTE_LEAK_DETECTED );
+    }
+<?php endif; ?>
 
 
     if !(avoid_martians()) then {
