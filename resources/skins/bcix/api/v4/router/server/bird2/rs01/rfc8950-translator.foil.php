cc9d395d94c7c954ebc727baef812468ed1055bb
Christopher Rössler <caroessler@aol.com>
2025-10-31 13:05:53 +0100

    adapt export filter in translator

--- resources/skins/bcix/api/v4/router/server/bird2/rs01/rfc8950-translator.foil.php
+++ resources/skins/bcix/api/v4/router/server/bird2/rs01/rfc8950-translator.foil.php
@@ -14,8 +14,11 @@ use IXP\Models\IPv6Address;
 ###
 ### AS4200008950 - BCIX RFC8950 translator01 - VLAN Interface #<?= $vlan_intf_id ?>
 
-filter f_export_as4200008950{
-    if ! (ixp_community_filter( 4200008950 ) ) then reject;
+filter f_export_as4200008950 {
+    if (routeserverasn, 0, 0) ~ bgp_large_community then
+        reject;
+    if (0, routeserverasn) ~ bgp_community then
+        reject;
 
     if bgp_large_community ~ [( routeserverasn, 1101, * )] then reject;
 
@@ -26,7 +29,7 @@ filter f_export_as4200008950{
     accept;
 }
 
-filter f_import_as4200008950{
+filter f_import_as4200008950 {
     bgp_path.delete( 4200008950 );
     accept;
 }
