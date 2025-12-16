e7fee030d679f62043cb8c4c924ed61029676b1a
Christopher Rössler <chris@rsslr.de>
2025-12-16 15:41:29 +0100

    fix bgp community problem with 32 bit ASNs

--- resources/skins/bcix/api/v4/router/server/bird2/rs03/rfc8950-translator.foil.php
+++ resources/skins/bcix/api/v4/router/server/bird2/rs03/rfc8950-translator.foil.php
@@ -1,25 +1,14 @@
-<?php
-use IXP\Models\VlanInterface;
-use IXP\Models\IPv4Address;
-use IXP\Models\IPv6Address;
-?>
-<?php $vlan_intf_id = 480 // insert vlaninterface id of translator here: 484 at rs01, 480 at rs02 ?>
-<?php $formatted_vlan_intf_id = sprintf( '%04d', $vlan_intf_id ) // vlaninterface id formatted ?>
-<?php $vlan_interface = VlanInterface::find($vlan_intf_id) ?>
-<?php $ipv4addr = IPv4Address::find($vlan_interface['ipv4addressid'])['address'] ?>
-<?php $ipv6addr = IPv6Address::find($vlan_interface['ipv6addressid'])['address'] ?>
 
 ########################################################################################
 ########################################################################################
 ###
-### AS4200008950 - BCIX RFC8950 translator01 - VLAN Interface #<?= $vlan_intf_id ?>
+### AS4200008950 - BCIX RFC8950 translator01 - VLAN Interface #480
 
-filter f_export_as4200008950 {
+filter f_export_as4200008950{
     if (routeserverasn, 0, 0) ~ bgp_large_community then
         reject;
     if (0, routeserverasn) ~ bgp_community then
         reject;
-
     if bgp_large_community ~ [( routeserverasn, 1101, * )] then reject;
 
     #####Graceful BGP Session Shutdown####
@@ -29,17 +18,17 @@ filter f_export_as4200008950 {
     accept;
 }
 
-filter f_import_as4200008950 {
+filter f_import_as4200008950{
     bgp_path.delete( 4200008950 );
     accept;
 }
 
-protocol bgp pb_<?= $formatted_vlan_intf_id ?>_as4200008950 from tb_rsclient {
+protocol bgp pb_0480_as4200008950 from tb_rsclient {
     description "AS4200008950 - BCIX RFC8950 translator01";
 <?php   if( $t->router->protocol == 6 ): ?>
-    neighbor <?= $ipv6addr ?> as 4200008950;
+    neighbor 2001:7f8:19:1::3ff6:fa as 4200008950;
 <?php   else: ?>
-    neighbor <?= $ipv4addr ?> as 4200008950;
+    neighbor 193.178.185.250 as 4200008950;
 <?php   endif; ?>
     ipv4 {
 <?php   if( $t->router->protocol == 6 ): ?>
@@ -56,9 +45,5 @@ protocol bgp pb_<?= $formatted_vlan_intf_id ?>_as4200008950 from tb_rsclient {
     };
     passive on;
     interpret communities off;  # enable rfc1997 well-known community pass through
-<?php   if( $t->router->protocol == 6 ): ?>
-    password "<?= $vlan_interface['ipv6bgpmd5secret'] ?>";
-<?php   else: ?>
-    password "<?= $vlan_interface['ipv4bgpmd5secret'] ?>";
-<?php   endif; ?>
+    password "6TUshwN6BdTR";
 }
